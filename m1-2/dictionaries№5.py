c5518a5adb39d813910869ec629df02eaddee6f5
Totoshk0 <Fatkulinsasha@gmail.com>
2025-04-02 00:06:58 +0300

    m1-2

---
+++ m1-2/dictionaries№5.py
@@ -0,0 +1,3 @@
+my_dict = {'a': 10, 'b': 20, 'c': 30}
+sum_values = sum(my_dict.values())
+print(sum_values)
---
+++ m1-2/dictionarise№8.py
@@ -0,0 +1,3 @@
+my_dict = {'a': 10, 'b': 20, 'c': 30, 'd': 100}
+val = sum(my_dict.values()) / len(my_dict)
+print (val)
